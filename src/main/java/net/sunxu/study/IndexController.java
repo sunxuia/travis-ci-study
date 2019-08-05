a3ee027ac189e9b665bc2a833e63424d1124e920
sunxu <ntsunxu@163.com>
2019-08-05 21:41:23 +0800

    add /test

--- src/main/java/net/sunxu/study/IndexController.java
+++ src/main/java/net/sunxu/study/IndexController.java
@@ -1,6 +1,7 @@
 package net.sunxu.study;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,10 @@ public class IndexController {
     @GetMapping("/")
     public String index() {
         return "ok";
+    }
+
+    @GetMapping("/test")
+    public String test(@RequestParam("paraKey") String paraKey) {
+        return paraKey;
     }
 }
