0c68947cb1cff1cfbcf940fa1f4556e83c95cc92
sunxu <ntsunxu@163.com>
2019-08-16 21:45:24 +0800

    add redis

--- src/main/java/net/sunxu/study/IndexController.java
+++ src/main/java/net/sunxu/study/IndexController.java
@@ -2,6 +2,7 @@ package net.sunxu.study;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,9 +14,14 @@ public class IndexController {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
     @GetMapping("/")
     public String index() {
         String res = jdbcTemplate.queryForObject("select 'ok'", String.class);
+        stringRedisTemplate.opsForValue().set("test-key", res);
+        res = stringRedisTemplate.opsForValue().get("test-key");
         log.info("got information " + res);
         return res;
     }
